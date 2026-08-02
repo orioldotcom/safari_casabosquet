ca5305f20d23dbabaf7c8517261e9e6d3c0b9139
Max Mayordomo <max@MacBook-Neo-de-Max.local>
2026-08-02 21:04:02 +0800

    Configura deploy automatic a GitHub Pages desde feature/v2-expedicions

--- vite.static.config.ts
+++ vite.static.config.ts
@@ -5,7 +5,7 @@ import { fileURLToPath } from "node:url";
 const root = fileURLToPath(new URL(".", import.meta.url));
 
 export default defineConfig({
-  base: "/safari/",
+  base: "/safari_casabosquet/",
   plugins: [react()],
   build: {
     outDir: "dist-static",
